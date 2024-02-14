68ddfe88b08caad3bbe68aac9372d65a46de9d71
eulyoung jung <81278853+jungeulyoung@users.noreply.github.com>
2024-02-14 18:26:23 +0900

    first commit

---
+++ src/main/java/org/example/password/RandomPasswordGenerator.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.passay.CharacterData;
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+
+public class RandomPasswordGenerator implements PasswordGenerator {
+
+    private static final String ALLOWED_SPL_CHARACTERS = "!@#$%^&*()_+";
+
+    private static final String ERROR_CODE = "ERRONEOUS_SPECIAL_CHARS";
+
+
+    public String generatePassword() {
+        org.passay.PasswordGenerator gen = new org.passay.PasswordGenerator();
+
+        CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
+        CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
+        lowerCaseRule.setNumberOfCharacters(2);
+
+        CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
+        CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
+        upperCaseRule.setNumberOfCharacters(2);
+
+        CharacterData digitChars = EnglishCharacterData.Digit;
+        CharacterRule digitRule = new CharacterRule(digitChars);
+        digitRule.setNumberOfCharacters(2);
+
+        CharacterData specialChars = new CharacterData() {
+            @Override
+            public String getErrorCode() {
+                return ERROR_CODE;
+            }
+
+            @Override
+            public String getCharacters() {
+                return ALLOWED_SPL_CHARACTERS;
+            }
+        };
+        CharacterRule splCharRule = new CharacterRule(specialChars);
+        splCharRule.setNumberOfCharacters(2);
+
+        return gen.generatePassword((int) (Math.random() * 13), splCharRule, lowerCaseRule, upperCaseRule, digitRule);
+    }
+
+}
